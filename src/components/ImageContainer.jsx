7117ab8fd421bdc63ec6771c9f5f46f9ecbe891f
Dylan Marley <dylanjmarley01@gmail.com>
2026-01-08 13:15:55 +0000

    feat: tab through images and enter to view

--- src/components/ImageContainer.jsx
+++ src/components/ImageContainer.jsx
@@ -3,12 +3,21 @@ export default function ImageContainer({ images, setImgIndex }) {
     setImgIndex(id - 1);
   }
 
+  const handleKeyDown = (event, id) => {
+    if (event.key === "Enter") {
+      console.log("Enter Pressed");
+      handleClick(id);
+    }
+  };
+
   return (
     <section className="thumbnail-container">
       {images.map((image) => (
         <div key={image.id}>
           <img
+            tabIndex={image.id}
             onClick={() => handleClick(image.id)}
+            onKeyDown={(event) => handleKeyDown(event, image.id)}
             src={image.url}
             alt={image.alt}
           />
